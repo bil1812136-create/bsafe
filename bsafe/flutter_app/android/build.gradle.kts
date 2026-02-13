832facaa27a9245526d781b90b1112fec8ef7a05
intern03 <intern03@bsafe.local>
2026-02-13 10:37:51 +0800

    v2.4.1

--- bsafe/flutter_app/android/build.gradle.kts
+++ bsafe/flutter_app/android/build.gradle.kts
@@ -19,6 +19,30 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Fix for older plugins missing namespace (e.g. flutter_libserialport)
+subprojects {
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            if (namespace == null || namespace!!.isEmpty()) {
+                // Read package from AndroidManifest.xml
+                val manifestFile = file("src/main/AndroidManifest.xml")
+                if (manifestFile.exists()) {
+                    val manifest = javax.xml.parsers.DocumentBuilderFactory.newInstance()
+                        .newDocumentBuilder().parse(manifestFile)
+                    val pkg = manifest.documentElement.getAttribute("package")
+                    if (pkg.isNotEmpty()) {
+                        namespace = pkg
+                    } else {
+                        namespace = "dev.flutter.${project.name.replace("-", "_")}"
+                    }
+                } else {
+                    namespace = "dev.flutter.${project.name.replace("-", "_")}"
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
